e52284e64dccc4fbe3521147f1255b39e11008c6
mjeffery <michael.jeffery@gmail.com>
2016-05-06 10:42:29 -0400

    partial implementation for rendering format

---
+++ samples/simple-3.rendering.js
@@ -0,0 +1,36 @@
+{
+	extent: 3,
+	cells: [
+		[{ 
+			label: '2-', 
+			walls: { top: true, right: false, down: true, left: true },
+			annotation: [],
+			value: undefined
+		 }, {
+			walls: { top: true, right: true, down: true, left: false }
+		 }, {
+			label: '2',
+			walls: { top: true, right: true, down: true, left: true }
+		 }],
+		[{
+			walls: {},
+			annotations: []
+		}, {
+			walls: {},
+			annotations: []
+		}, {
+			walls: {},
+			annotations: []
+		}],
+		[{
+			walls: {},
+			annotations: []
+		}, {
+			walls: {},
+			annotations: []
+		}, {
+			walls: {},
+			annotations: []
+		}]
+	]
+}
