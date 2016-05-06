cd7aa68899e860f8f3ddea0a74893b6d918256c8
sburba <github@samburba.com>
2016-05-06 14:23:34 -0400

    Use the storageToRender func!

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PuzzleGrid from './components/PuzzleGrid.jsx'
-import state from '../samples/simple-3.rendering';
+import state from '../samples/simple-3.storage';
+import storageToRender from './converters/storageToRender';
+
+var renderingState = storageToRender(state);
 
 ReactDOM.render(
     <div>
-		<PuzzleGrid state={state} />
+        <PuzzleGrid state={renderingState}/>
     </div>,
     document.getElementById('app')
 );
